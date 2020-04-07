09467899fc074f6884b543298ea79c54d223551d
SimonGaufreteau <simon.gaufreteau@gmail.com>
2020-04-07 11:50:43 +0200

    Javadoc + small corrections

--- src/parser_utils/FromHTML.java
+++ src/parser_utils/FromHTML.java
@@ -2,19 +2,32 @@ package parser_utils;
 
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
+/**
+ * A basic class using JSoup to gather text from a web page.
+ * @author GAUFRETEAU Simon
+ */
 public class FromHTML {
 
-    public static Elements queryFromHTMl(String url,String cssQuery) throws IOException {
-        Document doc = Jsoup.connect(url).get();
+
+    public static Elements queryFromHTML(String url,String cssQuery) throws IOException {
+        System.out.println("Waiting to connect to the url...");
+        long time = System.currentTimeMillis();
+        Connection connect = Jsoup.connect(url);
+        System.out.println("Connected to the url in "+(System.currentTimeMillis()-time)+" milliseconds.");
+        Document doc = connect.get();
         return queryFromHTML(doc,cssQuery);
     }
     public static Elements queryFromHTML(Document doc,String cssQuery){
         return doc.select(cssQuery);
+    }
+
+    public static String stringFromHTML(String url,String cssQuery) throws IOException {
+        Elements el = queryFromHTML(url,"p,h1,h2,h3");
+       return el.text();
     }
 
 }
